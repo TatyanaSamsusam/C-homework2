a8069279b81da60b8a3527757b8153f362a04363
Tatyana <android87@list.ru>
2022-05-13 21:54:18 +0400

    shows a second digit

---
+++ task_10_showSecondDigit/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// Примеры:
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("введите трехзначное число, и я покажу его вторую цифру");
+int number = int.Parse(Console.ReadLine());
+
+if(number > 99 && number < 1000) Console.WriteLine($"вы ввели число {number}");
+else Console.WriteLine("число не трехзначное!");
+
+int FindSecondDigit (int number)
+{
+    int sub = number / 10;
+    int finaldigit = sub % 10;
+    return finaldigit;
+}
+
+int secondDigitMethod = FindSecondDigit(number);
+Console.WriteLine($"вторая цифра этого числа: {secondDigitMethod}");
